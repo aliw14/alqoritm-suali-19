d5c292927cd3332a42fc64ee3585dad4e3c7c7d4
ali <ali@MacBook-Pro-Ali.local>
2023-01-04 03:04:02 +0400

    mesaj

---
+++ alqoritm suali 19/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace alqoritm_suali_19;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        int edediorta;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+
+
+
+
+
+
+        }
+        edediorta = sum / numbers.Length;
+        Console.WriteLine(edediorta);
+
+
+
+
+    }
+}
+
